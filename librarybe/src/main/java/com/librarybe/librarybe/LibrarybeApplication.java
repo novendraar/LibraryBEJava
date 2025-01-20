344b9c5e5d437ab324f00ae7502e0aa61c522a57
novendraar <novendraar@gmail.com>
2025-01-20 10:05:30 +0700

    Initialisation

---
+++ librarybe/src/main/java/com/librarybe/librarybe/LibrarybeApplication.java
@@ -0,0 +1,13 @@
+package com.librarybe.librarybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarybeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarybeApplication.class, args);
+	}
+
+}
